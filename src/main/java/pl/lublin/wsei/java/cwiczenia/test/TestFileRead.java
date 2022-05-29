d584798c3d74f3cdfe8d580b7026d0a9b7ae52ad
Maciej Kosakowski <kosakowski222@gmail.com>
2022-05-29 13:50:21 +0200

    Punkt 8, zastosowanie polecenia wczytującego plik gusInfoGraphic.xml

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestFileRead.java
@@ -0,0 +1,21 @@
+package pl.lublin.wsei.java.cwiczenia.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class TestFileRead {
+    public static void main(String[] args) {
+        try {
+            String contents = new String(Files.readAllBytes(Paths.get("gusInfoGraphic.xml")));
+            System.out.println("ZAWARTOSC PLIKU gunInfoGrafic.xml");
+            System.out.print(contents);
+        }
+        catch (IOException e) {
+            System.out.println("Błąd wczytywania pliku gusInfoGraphic.xml => "+ e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+
+    }
+}
