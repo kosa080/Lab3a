3567a22a8592b9a744231a28e107fe933f80a4fb
Maciej Kosakowski <kosakowski222@gmail.com>
2022-06-13 14:54:45 +0200

    Lab 3b. Punkt 7 Utworzono nowy plik o nazwie imgViewer.fxml oraz wprowadzono kod dzięki któremu po naciśnięciu klawisza ‘’Pokaż” w nowym oknie zostanie wyświetlona miniaturka infografiki.

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -7,6 +7,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -17,6 +20,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Controller {
 
@@ -85,4 +89,27 @@ public class Controller {
         if (selInfografika != null)
             hostServices.showDocument(selInfografika.strona);
     }
+
+    public void btnPokaz(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("imgViewer.fxml"));
+            Parent root = loader.load();
+            ImgViewer viewer = loader.getController();
+            if (selInfografika != null) {
+                Image img = new Image(selInfografika.grafika);
+                viewer.imgView.setFitWidth(img.getWidth());
+                viewer.imgView.setFitHeight(img.getHeight());
+                viewer.imgView.setImage(img);
+            }
+
+            Stage stage = new Stage();
+            stage.setTitle("Podgląd infografiki");
+            stage.setScene(new Scene(root, 900, 800));
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
