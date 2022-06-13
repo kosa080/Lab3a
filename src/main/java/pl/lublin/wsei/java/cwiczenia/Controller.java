26648fb4c6aaa52a774f18ec7702d4589011af7b
Maciej Kosakowski <kosakowski222@gmail.com>
2022-06-13 10:24:58 +0200

    Lab 3b. Punkt 5 Dodane zostały kontrolki Label, ImageView, TextField oraz Button, każdej został ustawiony identyfikator fx:id. Po wprowadzeniu kodu kontrolka ImageView wyświetla miniaturkę a TextField adres strony.

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,17 +1,29 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 
 public class Controller {
+
     public Label lbFile;
+    public ImageView imgMiniaturka;
+    public TextField txtAdresStrony;
+    public Button btnPokazInfografike;
+    public Button btnPrzejdzDoStrony;
+
     FileChooser fileChooser = new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)","*.xml");
 
@@ -22,6 +34,23 @@ public class Controller {
     @FXML
     public void initialize() {
         fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(
+            new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> observableValue, Number old_val, Number new_val) {
+                    int index = new_val.intValue();
+                    if (index != -1) {
+                        txtAdresStrony.setText(igList.infografiki.get(index).strona);
+                        Image image = new Image(igList.infografiki.get(index).miniatura);
+                        imgMiniaturka.setImage(image);
+                    }
+                    else {
+                        txtAdresStrony.setText("");
+                        imgMiniaturka.setImage(null);
+                    }
+                }
+            }
+        );
     }
 
     public void btnOpenFileAction(ActionEvent actionEvent) {
